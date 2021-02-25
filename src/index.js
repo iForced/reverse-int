9e62bc43c1efe97e42afd4c33fb841c9a1dd0008
iForced <iforce@2ch.hk>
2021-02-25 23:00:43 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let absN = Math.abs(n);
+    let arr = [absN].join().split('').reverse();
+    let numString = '';
+
+    arr.forEach(item => {
+        numString += item;
+    })
+    return Number(numString);
 }
